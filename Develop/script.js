365c1566160217601eb96a3bd8318b7c9e6d2be9
Clayton Andrews <claytonandrews897@gmail.com>
2024-02-19 19:08:17 -0500

    edited the js and html

--- Develop/script.js
+++ Develop/script.js
@@ -1,41 +1,39 @@
+
 $(document).ready(function () {
-  // console.log("just testing");
-  //define my variables for today's date and the time right now
-  let currentDay = moment().format('LLLL');
-  let currentTime = (moment().hour());
+    // Define variables for today's date and the time right now
+    let currentDay = moment().format('dddd, MMMM Do YYYY'); // Include day of the week
+     // Update the text content of the #currentDay element with the current day
+    $("#currentDay").text(currentDay);
+    let currentTime = moment().hour();
 
-  //update elements with localStorage values
-  for (key = 0; key < localStorage.length; key++){
-      timeID = localStorage.key(key);
-      timeValue = localStorage.getItem(timeID);
-      $("#"+timeID).val(timeValue);
-  }
-  //display today's date in the currentDate id
-  $("#currentDay").append(currentDay);
-  $("input").each(function () {
-      var item = parseInt($(this).attr("time"));
+    // Update elements with localStorage values
+    for (key = 0; key < localStorage.length; key++){
+        timeID = localStorage.key(key);
+        timeValue = localStorage.getItem(timeID);
+        $("#"+timeID).val(timeValue);
+    }
 
-      console.log(item)
-      // console.log(currentTime)
-      if (item < currentTime) {
-          $(this).addClass("past");
-      }
-      if (item > currentTime) {
-          $(this).addClass("present")
-      }
+    // Display today's date with the day of the week in the currentDate id
+    $("#currentDay").append(currentDay);
 
-      if (item === currentTime) {
-          $(this).addClass("future")
-      }
-  });
-  $("button").on("click", function () {
-      //assigning the value to from input to variable
-      inputID = $(this).attr("input-id");
-      inputEl = $(document.getElementById(inputID))
-      // inputEl = $($(this).parent().children()[0]);
-      calendarEvent = inputEl.val();
-      console.log($(this).val())
-      console.log($(this).parent)
-      localStorage.setItem(inputEl.attr("id"), calendarEvent);
-  })
+    // Add styling based on past, present, and future times
+    $("input").each(function () {
+        var item = parseInt($(this).attr("time"));
+
+        if (item < currentTime) {
+            $(this).addClass("past");
+        } else if (item > currentTime) {
+            $(this).addClass("future");
+        } else {
+            $(this).addClass("present");
+        }
+    });
+
+    // Save event to localStorage when the save button is clicked
+    $("button").on("click", function () {
+        inputID = $(this).attr("input-id");
+        inputEl = $(document.getElementById(inputID));
+        calendarEvent = inputEl.val();
+        localStorage.setItem(inputEl.attr("id"), calendarEvent);
+    });
 });
